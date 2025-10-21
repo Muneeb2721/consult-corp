1f28389ad045bcce2df6168a63f42b827deea6f2
Muneeb2721 <muneebnadeem1870@gmail.com>
2025-10-21 21:16:40 +0500

    (feat): import About, CaseStudies components in default page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import Navbar from "./components/Navbar";
 import Demo from "./components/Demo";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import CaseStudies from "./components/casestudies";
 
 export default function Project () {
     return (
         <main>
             <Navbar />
+            <About />
+            <CaseStudies />
             <Demo />
             <Footer />
+
         </main>
     )
 
