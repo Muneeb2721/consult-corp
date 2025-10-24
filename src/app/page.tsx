5d1752563528fb85d242e799823aa28c6bf6f4be
Muneeb2721 <muneebnadeem1870@gmail.com>
2025-10-24 22:45:13 +0500

    (feat): import Hero and  Insights components in default page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Insights from "./components/Insights";
+import CaseStudies from "./components/casestudies";
 import Demo from "./components/Demo";
 import Footer from "./components/Footer";
-import About from "./components/About";
-import CaseStudies from "./components/casestudies";
 
 export default function Project () {
     return (
         <main>
             <Navbar />
+            <Hero />
             <About />
+            <Insights />
             <CaseStudies />
             <Demo />
             <Footer />
 
         </main>
     )
-
 }
