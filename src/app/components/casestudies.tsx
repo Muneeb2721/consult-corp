8e84ea36662e09e83c4a2348ae6d36fe2ad45e6d
Muneeb2721 <muneebnadeem1870@gmail.com>
2025-10-25 03:19:16 +0500

    Fixed JSX unescaped entities for deployment

--- src/app/components/casestudies.tsx
+++ src/app/components/casestudies.tsx
@@ -8,7 +8,7 @@ export default function CaseStudies() {
             <h1 className='font-semibold pt-10  text-center
             sm:text-[28px] 
             text-[24px]'>Featured Case Studies</h1>
-            <p className='text-center'>Explore how we've helped organizations achieve remarkable results</p>
+            <p className='text-center'>Explore how we&apos;ve helped organizations achieve remarkable results</p>
 
             <div className="flex gap-4 py-15
             md:flex-row
@@ -28,7 +28,7 @@ export default function CaseStudies() {
                     <p className='px-[25px]'>Helped a leading technology company modernize their infrastructure, resulting in 45% improved efficiency.</p>
 
                     <div className="link flex justify-end mr-5 mb-5">
-                        <a href="#" className='py-2 px-5 bg-[#0070f3] text-white rounded'>Read more</a>
+                        <a href="#" className='py-2 px-5 bg-blue-700 text-white text-[18px] rounded'>Read more</a>
                     </div>
                 </div>
                 
@@ -46,7 +46,7 @@ export default function CaseStudies() {
                     <p className='px-[25px]'>Developed a 5-year growth strategy for a financial institution, driving 28% revenue increase.</p>
 
                     <div className="link flex justify-end mr-5 mb-5">
-                        <a href="#" className='py-2 px-5 bg-[#0070f3] text-white rounded'>Read more</a>
+                        <a href="#" className='py-2 px-5 bg-blue-700 text-white text-[18px] rounded'>Read more</a>
                     </div>
                 </div>
 
@@ -64,7 +64,7 @@ export default function CaseStudies() {
                     <p className='px-[25px]'>Streamlined operations for a global manufacturer, reducing costs by 32% while improving quality.</p>
 
                     <div className="link flex justify-end mr-5 mb-5">
-                        <a href="#" className='py-2 px-5 bg-[#0070f3] text-white rounded'>Read more</a>
+                        <a href="#" className='py-2 px-5 bg-blue-700 text-white text-[18px] rounded'>Read more</a>
                     </div>
                 </div>
             </div>
